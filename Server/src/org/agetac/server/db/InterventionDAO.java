8089c7fa81ca5eba4fd9a8c3af1e089719243dda
Georgios Politis <gp@omega.(none)>
2012-03-28 19:08:23 +0200

    adds mistakenly ignored DAO classes

---
+++ Server/src/org/agetac/server/db/InterventionDAO.java
@@ -0,0 +1,199 @@
+package org.agetac.server.db;
+
+import java.util.Collection;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Transaction;
+
+import org.agetac.common.dto.*;
+
+
+public class InterventionDAO {
+
+	public void add(long interId, VehicleDemand vehicleDemand) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			inter.getVehicleDemands().add(vehicleDemand);
+			pm.makePersistent(inter);
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+	public void add(long interId, Message message) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			inter.getMessages().add(message);
+			pm.makePersistent(inter);
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+	public void add(long interId, Source source) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			inter.getSources().add(source);
+			pm.makePersistent(inter);
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+	public void add(long interId, Target target) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			inter.getTargets().add(target);
+			pm.makePersistent(inter);
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+
+	}
+
+	public Collection<VehicleDemand> retrieveVehicleDemands(long interId) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		try {
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			return inter.getVehicleDemands();
+		} finally {
+
+			pm.close();
+		}
+
+	}
+
+	public Collection<Target> retrieveTargets(long interId) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		try {
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			return inter.getTargets();
+		} finally {
+
+			pm.close();
+		}
+	}
+
+	public Collection<Message> retrieveMessages(long interId) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		try {
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			return inter.getMessages();
+		} finally {
+
+			pm.close();
+		}
+	}
+
+	public Collection<Source> retrieveSources(long interId) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		try {
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			return inter.getSources();
+		} finally {
+
+			pm.close();
+		}
+	}
+
+	public void add(long interId, Victim involved) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			inter.getVictims().add(involved);
+			pm.makePersistent(inter);
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+	public Collection<Victim> retrieveVictims(long interId) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		try {
+
+			Object interKey = pm.newObjectIdInstance(Intervention.class,
+					interId);
+
+			Intervention inter = (Intervention) pm.getObjectById(interKey);
+			return inter.getVictims();
+		} finally {
+
+			pm.close();
+		}
+	}
+}
---
+++ Server/src/org/agetac/server/db/SimpleDAO.java
@@ -0,0 +1,112 @@
+package org.agetac.server.db;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+
+public class SimpleDAO {
+
+	private static PersistenceManagerFactory pmfInstance;
+
+	private static PersistenceManagerFactory getPMF() {
+
+		if (pmfInstance == null)
+			pmfInstance = JDOHelper
+					.getPersistenceManagerFactory("jdo.properties");
+
+		return pmfInstance;
+	}
+
+	private static SimpleDAO daoInstance;
+
+	public static SimpleDAO getInstance() {
+		if (daoInstance == null)
+			// TODO Use factories?
+			daoInstance = new SimpleDAO();
+
+		return daoInstance;
+	}
+
+	public static PersistenceManager getPM() {
+		return getPMF().getPersistenceManager();
+	}
+
+	public synchronized <T> void add(T objTransient) {
+		PersistenceManager pm = getPM();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			pm.makePersistent(objTransient);
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+	public synchronized <T> T getOne(Class<T> cls, String uid) {
+		PersistenceManager pm = getPM();
+
+		try {
+			Object idInstance = pm.newObjectIdInstance(cls, uid);
+
+			T obj = (T) pm.getObjectById(idInstance);
+			return obj;
+		} catch (Exception ex) {
+			return null;
+		}
+
+	}
+
+	public synchronized <T> void delete(Class<T> cls, String uniqueID) {
+		PersistenceManager pm = SimpleDAO.getPM();
+
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+
+			Object idInstance = pm.newObjectIdInstance(cls, uniqueID);
+			@SuppressWarnings("unchecked")
+			T obj = (T) pm.getObjectById(idInstance);
+			if (obj == null)
+				return;
+
+			pm.deletePersistent(obj);
+
+			tx.commit();
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+	public synchronized <T> Collection<T> getMany(Class<T> cls) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Query query = pm.newQuery(cls);
+
+		@SuppressWarnings("unchecked")
+		Collection<T> results = (Collection<T>) query.execute();
+		return results;
+	}
+
+	public <T> List<T> getMany(Class<T> cls, String q) {
+		PersistenceManager pm = SimpleDAO.getPM();
+		Query query = pm.newQuery(cls, q);
+
+		@SuppressWarnings("unchecked")
+		List<T> results = (List<T>) query.execute();
+		return results;
+	}
+
+}
